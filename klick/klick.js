469021472b58497c996e631f31ca442186cbb523
Alvin Lin <alvin.lin@stuypulse.com>
2014-10-20 23:41:09 -0400

    Successfully made physics work.

--- klick/klick.js
+++ klick/klick.js
@@ -6,17 +6,28 @@
 
 function Klick(canvas) {
   this.canvas_ = canvas;
+  this.circle_ = null;
+  this.circleModel_ = null;
 }
 
 Klick.prototype.update = function(event) {
   var mouseX = event.pageX - this.canvas_.parentElement.offsetLeft;
   var mouseY = event.pageY - this.canvas_.parentElement.offsetTop;
-
-  console.log(mouseX, mouseY);
+  this.circle_.setXY(this.circleModel_.getXY());
 };
 
 Klick.prototype.startGame = function() {
-  this.canvas_.onmousemove = bind(this, function(e) {
-    this.update(e);
+  this.circle_ = new Circle(50, 350, 10, 'red');
+  this.circleModel_ = new PhysicalObjectModel(50, 350, 0, 0, null, null, 350);
+  this.canvas_.appendChild(this.circle_.getSVG());
+
+  this.canvas_.onmousedown = bind(this, function() {
+    this.circleModel_.setVY(-100);
   });
+
+  setInterval(bind(this, function() {
+    this.circle_.setXY(this.circleModel_.getXY());
+    this.circleModel_.update();
+
+  }), 1);
 };
