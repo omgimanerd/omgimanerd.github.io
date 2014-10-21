ead1f77bcb2beb7c8ca900e0a53e2fa954b51f7a
Alvin Lin <alvin.lin@stuypulse.com>
2014-10-21 14:08:26 -0400

    Created basic structure for klick.

--- klick/klick.js
+++ klick/klick.js
@@ -4,30 +4,67 @@
  * Script for the klick game.
  */
 
-function Klick(canvas) {
+function Klick(canvas, klickOverlayEl, scoreEl, highScoreEl) {
   this.canvas_ = canvas;
-  this.circle_ = null;
-  this.circleModel_ = null;
+  this.width_ = canvas.offsetWidth;
+  this.height_ = canvas.offsetHeight;
+  this.overlayEl_ = klickOverlayEl;
+  this.scoreEl_ = scoreEl;
+  this.highScoreEl_ = highScoreEl;
+
+  // Necessary global variables.
+  this.score_ = 0;
+  this.gameLoop_ = null;
+  this.lost_ = true;
 }
 
-Klick.prototype.update = function(event) {
-  var mouseX = event.pageX - this.canvas_.parentElement.offsetLeft;
-  var mouseY = event.pageY - this.canvas_.parentElement.offsetTop;
-  this.circle_.setXY(this.circleModel_.getXY());
+Klick.prototype.buildGameStart = function() {
+  // Necessary SVG elements and the physics model of the moving
+  // circle.
+  this.circle_ = new Circle(100, 350, 10, 'red');
+  this.circle_.addModel(new PhysicalObjectModel(
+      100, 350, 0, 0, null, null, 375));
+
+  // Create the background.
+  this.background_ = new Rect(
+      0, 0, this.width_, this.height_, Colors.KLICK_BACKGROUND);
+  this.canvas_.appendChild(this.background_.getSVG());
+};
+
+Klick.prototype.update = function() {
 };
 
 Klick.prototype.startGame = function() {
-  this.circle_ = new Circle(50, 350, 10, 'red');
-  this.circleModel_ = new PhysicalObjectModel(50, 350, 0, 0, null, null, 350);
+  // Add the circle to the canvas.
   this.canvas_.appendChild(this.circle_.getSVG());
 
+  // Add the canvas event handler.
   this.canvas_.onmousedown = bind(this, function() {
-    this.circleModel_.setVY(-100);
+    this.circle_.setVY(this.circle_.getVY() + 100);
   });
 
-  setInterval(bind(this, function() {
-    this.circle_.setXY(this.circleModel_.getXY());
-    this.circleModel_.update();
+  // Start the game loop.
+  if (this.lost_) {
+    setInterval(bind(this, function() {
+      this.circle_.updateWithPhysics();
+    }), 1);
+  }
+  this.lost_ = false;
 
-  }), 1);
+  // Hide the overlay.
+  this.overlayEl_.style.zIndex = -1;
+};
+
+Klick.prototype.endGame = function() {
+  // TODO: insert cookie score counting
+
+  // Stop the game loop.
+  this.lost_ = true;
+  clearInterval(this.gameLoop_);
+  this.canvas_.removeChild(this.circle_.getSVG());
+
+  // Bring back the overlay.
+  this.overlayEl_.style.lineHeight = '100px;';
+  this.overlayEl_.style.zIndex = 1;
+  this.overlayEl_.style.innerHTML = 'You lost!<br />Try again';
 };
