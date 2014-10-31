461c7de0ef1c879223b019f82d1ee3015baa21d9
Alvin Lin <alvin.lin@stuypulse.com>
2014-10-31 11:35:55 -0400

    updated doc

--- tap/tap.js
+++ tap/tap.js
@@ -180,7 +180,7 @@ Tap.prototype.createRandomDot = function() {
 
   this.createDot(x, y, radius, wavelength, amplitude, color, speed);
 
-  // Additional 10% chance that an extra dot will be generated.
+  // Additional 15% chance that an extra dot will be generated.
   if (Math.random() < 0.15) {
     this.createRandomDot();
   }
