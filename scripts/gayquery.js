3c6053be7dbd61718839949bbde6750393dc7daf
Alvin Lin <alvin.lin@stuypulse.com>
2015-04-07 16:13:10 -0400

    scroll test

--- scripts/gayquery.js
+++ scripts/gayquery.js
@@ -1,21 +1,8 @@
-scroll_to($("anchor0"))
-var lastScrollTop = $(window).scrollTop();
-var NUM_PAGES = 3;
-var currentPage = 0;
-var scrolling = false;
-
 function scroll_to(div) {
   scrolling = true;
-  $('html, body').animate(
-    {
-      scrollTop: div.offset().top
-    },
-    1000,
-    null,
-    function() {
-      scrolling = false;
-    }
-  );
+  $('html, body').animate({
+    scrollTop: div.offset().top
+  }, 1000);
 }
 
 $('a[href^="#"]').on('click', function(event) {
@@ -23,15 +10,4 @@ $('a[href^="#"]').on('click', function(event) {
   event.preventDefault();
   scroll_to($(target));
   currentPage = target.substr(0, -1);
-  console.log(currentPage);
-});
-
-$(window).scroll(function(event) {
-  if (!scrolling) {
-    if ($(this).scrollTop() > lastScrollTop) {
-      scroll_to($("anchor" + (currentPage + 1 % 3)));
-    } else {
-    }
-    lastScrollTop = $(this).scrollTop();
-  }
 });
