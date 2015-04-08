7f0ac339e3e2ed80d1f8483b6b50ac82bb98aec8
Alvin Lin <alvin.lin@stuypulse.com>
2015-04-08 09:48:54 -0400

    scroll testing

--- scripts/gayquery.js
+++ scripts/gayquery.js
@@ -1,8 +1,11 @@
+var isScrolling = false;
+var lastScrollTop = $(document).scrollTop;
+
 function scroll_to(div) {
   $('html, body').animate({
     scrollTop: div.offset().top
   }, 1000, null, function() {
-    console.log("finished");
+    isScrolling = true;
   });
 }
 
@@ -12,9 +15,14 @@ $('a[href^="#"]').click(function(event) {
   scroll_to($(target));
 });
 
-/*
+
 var lastScrollTop = $(document).scrollTop;
 $(document).scroll(function() {
-  if ($(document).scrollTop > lastScrollTop) {
-    console.log('scrolled down')
-    */
+  if (!isScrolling) {
+    if ($(document).scrollTop > lastScrollTop) {
+      console.log('scrolled down');
+    } else {
+      console.log('scrolled up');
+    }
+  }
+});
