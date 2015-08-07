0d500ce90c9987b40cbb62c44c7bd4b9860e2d6f
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-06 23:53:00 -0400

    data

--- scripts/yayquery.js
+++ scripts/yayquery.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
   $('#name-fadein').hide().delay(750).fadeIn(1000);
   $('#description-fadein').hide().delay(1250).fadeIn(1000);
+
+  $('#bouncy-square-link').on('mouseover', function() {
+  });
 });
