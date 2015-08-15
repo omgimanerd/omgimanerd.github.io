13e40ac61fd284c8af2d190ec4a6e4a93382f044
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-15 00:44:50 -0400

    added documentation, credits, and swag :)

--- scripts/client.js
+++ scripts/client.js
@@ -3,11 +3,15 @@
  */
 
 $(document).ready(function() {
+  // Create and initialize a CursorAnimator
   var cursorAnimator = new CursorAnimator(
       document.getElementById('cursor-canvas'));
   cursorAnimator.initialize();
 
-  $(function () {
+  // Name fade-in function.
+  // Credit to Roy J (Stack Overflow):
+  // http://jsfiddle.net/bGsa3/6/
+  $(function() {
     var string = "Alvin Lin";
     var dest = $('#name');
     var c = 0;
@@ -15,6 +19,7 @@ $(document).ready(function() {
       if (c >= string.length) {
         clearInterval(i);
         dest.text(string);
+        // Set the cursor animator's event handlers when the name animation is done.
         cursorAnimator.setEventHandlers();
       } else {
         $('<span>').text(string[c]).
