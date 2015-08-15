07ee179372c25717978429976f18d4ebb713ab67
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-15 00:40:47 -0400

    added new cursor image, made fade-in name title bar

--- scripts/client.js
+++ scripts/client.js
@@ -3,6 +3,24 @@
  */
 
 $(document).ready(function() {
-  var cursor = new Cursor(document.getElementById('cursor-canvas'));
-  cursor.initialize();
+  var cursorAnimator = new CursorAnimator(
+      document.getElementById('cursor-canvas'));
+  cursorAnimator.initialize();
+
+  $(function () {
+    var string = "Alvin Lin";
+    var dest = $('#name');
+    var c = 0;
+    var i = setInterval(function() {
+      if (c >= string.length) {
+        clearInterval(i);
+        dest.text(string);
+        cursorAnimator.setEventHandlers();
+      } else {
+        $('<span>').text(string[c]).
+        appendTo(dest).hide().fadeIn(500);
+        c += 1;
+      }
+    }, 250);
+  });
 });
--- scripts/cursor.js
+++ scripts/cursor.js
@@ -4,55 +4,72 @@
  * @author Alvin Lin (alvin.lin@stuypulse.com)
  */
 
-function Cursor(canvas) {
+function CursorAnimator(canvas) {
   this.canvas_ = canvas;
   this.context_ = null;
 
   this.cursorImage_ = null;
   this.cursorCurrent_ = [0, 0];
   this.cursorTarget_ = [0, 0];
+
+  this.cursorRotationAngle_ = 0;
 }
 
 /**
  * This is an inertial constant that determines how fast the cursor image
  * follows the user's cursor.
  */
-Cursor.RESPONSIVENESS = 0.1;
+CursorAnimator.RESPONSIVENESS = 0.1;
 
 /**
  * This is the framerate / update delay in milliseconds.
  */
-Cursor.UPDATE_DELAY = 10;
+CursorAnimator.UPDATE_DELAY = 10;
+
+/**
+ * This is the rotation rate of the cursor image in radians per
+ * CursorAnimator.UPDATE_DELAY.
+ */
+CursorAnimator.ROTATION_RATE = 0.025
+
+/**
+ * This is the path to the image used for the cursor.
+ */
+CursorAnimator.IMAGE_SRC = 'data/cursor.png';
 
 /**
  * This is the color to set gradient lines for the cursor.
  */
-Cursor.LINE_COLOR = '#00E4FF';
+CursorAnimator.LINE_COLOR = '#00E4FF';
 
 /**
- * Initializes the canvas and canvas context and binds the event listeners
- * and update frames.
+ * This function initializes the canvas, canvas context, and cursor image.
  */
-Cursor.prototype.initialize = function() {
+CursorAnimator.prototype.initialize = function() {
   this.canvas_.style.position = 'fixed';
   this.updateCanvasSize();
-  this.cursorCurrent_ = [this.canvas_.width / 2, this.canvas_.height / 2];
+  this.cursorCurrent_ = [this.canvas_.width / 2, -1000];
   this.cursorTarget_ = [this.canvas_.width / 2, this.canvas_.height / 2];
 
   this.context_ = this.canvas_.getContext('2d');
 
   this.cursorImage_ = new Image();
-  this.cursorImage_.src = 'data/cursor.png';
+  this.cursorImage_.src = CursorAnimator.IMAGE_SRC;
+};
 
+/**
+ * This function sets the event handlers and the animation delay.
+ */
+CursorAnimator.prototype.setEventHandlers = function() {
   window.addEventListener('mousemove', bind(this, this.onMouseMove));
-  setInterval(bind(this, this.updateCursor), Cursor.UPDATE_DELAY);
+  setInterval(bind(this, this.updateAnimation), CursorAnimator.UPDATE_DELAY);
 };
 
 /**
  * This method fits the canvas to the document body. It is used to initialize
  * the canvas and set its size if the user resizes the window.
  */
-Cursor.prototype.updateCanvasSize = function() {
+CursorAnimator.prototype.updateCanvasSize = function() {
   this.canvas_.width = document.body.offsetWidth;
   this.canvas_.height = document.body.offsetHeight;
   this.canvas_.style.width = document.body.offsetWidth + 'px';
@@ -63,7 +80,7 @@ Cursor.prototype.updateCanvasSize = function() {
  * This method is bound to the mousemove event and only serves to record
  * the user's mouse position.
  */
-Cursor.prototype.onMouseMove = function(event) {
+CursorAnimator.prototype.onMouseMove = function(event) {
   this.cursorTarget_ = [event.clientX, event.clientY];
 };
 
@@ -71,11 +88,11 @@ Cursor.prototype.onMouseMove = function(event) {
  * This method is the actual animation method that animates the user's
  * crosshair.
  */
-Cursor.prototype.updateCursor = function() {
+CursorAnimator.prototype.updateAnimation = function() {
   this.cursorCurrent_[0] += (this.cursorTarget_[0] - this.cursorCurrent_[0]) *
-      Cursor.RESPONSIVENESS;
+      CursorAnimator.RESPONSIVENESS;
   this.cursorCurrent_[1] += (this.cursorTarget_[1] - this.cursorCurrent_[1]) *
-      Cursor.RESPONSIVENESS;
+      CursorAnimator.RESPONSIVENESS;
 
   // We call updateCanvasSize() in case the user resizes their window.
   this.updateCanvasSize();
@@ -91,22 +108,28 @@ Cursor.prototype.updateCursor = function() {
   this.drawCursorLine(0, this.canvas_.height, x, y);
   this.drawCursorLine(this.canvas_.width, this.canvas_.height, x, y);
 
+  this.context_.save();
+  this.context_.translate(this.cursorCurrent_[0], this.cursorCurrent_[1]);
+  this.context_.rotate(this.cursorRotationAngle_);
   // Draw the cursor image.
   this.context_.drawImage(
       this.cursorImage_,
-      this.cursorCurrent_[0] - this.cursorImage_.width / 2,
-      this.cursorCurrent_[1] - this.cursorImage_.height / 2);
+      -this.cursorImage_.width / 2,
+      -this.cursorImage_.height / 2);
+  this.context_.restore();
+
+  this.cursorRotationAngle_ += 0.025;
 };
 
 /**
  * This is an internal helper method to draw the lines from the corner to the
  * mouse.
  */
-Cursor.prototype.drawCursorLine = function(startX, startY, endX, endY) {
+CursorAnimator.prototype.drawCursorLine = function(startX, startY, endX, endY) {
   var gradient = this.context_.createLinearGradient(
       startX, startY, endX, endY);
   gradient.addColorStop(0, '#000000');
-  gradient.addColorStop(0.4, Cursor.LINE_COLOR);
+  gradient.addColorStop(0.4, CursorAnimator.LINE_COLOR);
   gradient.addColorStop(0.8, '#000000');
   this.context_.strokeStyle = gradient;
   this.context_.beginPath();
