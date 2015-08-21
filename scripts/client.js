d6a0342b8380648b64bff5188c3a2ef622a7f380
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-21 19:31:45 -0400

    Lightened stat color scheme and removed unnecesary js function from client

--- scripts/client.js
+++ scripts/client.js
@@ -12,24 +12,22 @@ $(document).ready(function() {
   // Name fade-in function.
   // Credit to Roy J (Stack Overflow):
   // http://jsfiddle.net/bGsa3/6/
-  $(function() {
-    var string = "Alvin Lin";
-    var dest = $('#name');
-    var charCounter = 0;
-    var i = setInterval(function() {
-      if (charCounter >= string.length) {
-        clearInterval(i);
-        dest.text(string);
-        // Set the cursor animator's event handlers 1s after the name animation
-        // is done.
-        setTimeout(function() {
-          cursorAnimator.setEventHandlers();
-        }, 1000);
-      } else {
-        $('<span>').text(string[charCounter]).
-        appendTo(dest).hide().fadeIn(400);
-        charCounter += 1;
-      }
-    }, 200);
-  });
+  var string = "Alvin Lin";
+  var dest = $('#name');
+  var charCounter = 0;
+  var i = setInterval(function() {
+    if (charCounter >= string.length) {
+      clearInterval(i);
+      dest.text(string);
+      // Set the cursor animator's event handlers 1s after the name animation
+      // is done.
+      setTimeout(function() {
+        cursorAnimator.setEventHandlers();
+      }, 1000);
+    } else {
+      $('<span>').text(string[charCounter]).
+      appendTo(dest).hide().fadeIn(400);
+      charCounter += 1;
+    }
+  }, 200);
 });
