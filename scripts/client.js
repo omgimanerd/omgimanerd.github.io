898f3ecc9cc217cb79632778be48c9b5fc4db1d9
Alvin Lin <alvin.lin@stuypulse.com>
2015-09-05 00:07:47 -0400

    created portfolio and skills overlays

--- scripts/client.js
+++ scripts/client.js
@@ -3,6 +3,10 @@
  * @author Alvin Lin (alvin.lin@stuypulse.com)
  */
 
+window.oncontextmenu = function() {
+  return false;
+}
+
 $(document).ready(function() {
   // Create and initialize a CursorAnimator
   var cursorAnimator = new CursorAnimator(
@@ -14,6 +18,9 @@ $(document).ready(function() {
   var i = 1;
   var charCounter = 0;
   var interval = setInterval(function() {
+    if (charCounter >= string.length) {
+      clearInterval(interval);
+    }
     $('#name').text(string.substr(0, charCounter) +
                  generateRandomAlphaNum(string.length - charCounter));
     ++i;
@@ -24,8 +31,29 @@ $(document).ready(function() {
   setTimeout(function() {
     cursorAnimator.setEventHandlers();
   }, 2500);
-});
 
-window.oncontextmenu = function() {
-  return false;
-}
+  // JQuery code to bind click and animations to HTML DOM elements.
+  $('.portfolio-link').click(function() {
+    $('.portfolio-overlay').animate({
+      top: '0'
+    }, 1000);
+  });
+
+  $('.portfolio-overlay > .close').click(function() {
+    $('.portfolio-overlay').animate({
+      top: '-100%'
+    }, 1000);
+  });
+
+  $('.skills-link').click(function() {
+    $('.skills-overlay').animate({
+      top: '0'
+    }, 1000);
+  });
+
+  $('.skills-overlay > .close').click(function() {
+    $('.skills-overlay').animate({
+      top: '-100%'
+    }, 1000);
+  });
+});
