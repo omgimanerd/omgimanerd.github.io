b2bc2d24ba763b69e6d7cd006f9822318e6025fb
Alvin Lin <alvin.lin@stuypulse.com>
2015-09-21 17:08:11 -0400

    hahahaahaha gulp is awesome

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,15 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
+var notify = require('gulp-notify');
 
 gulp.task('less', function() {
-  return gulp.src('./static/less/styles.less')
+  return gulp.src('./style/styles.less')
     .pipe(less({ compress: true}))
     .pipe(autoprefixer())
     .pipe(minifyCss())
-    .pipe(gulp.dest('./static/css'));
+    .pipe(gulp.dest('./style'))
+    .pipe(notify('LESS compiled and minified'));
 });
 
 gulp.task('watch-less', function() {
