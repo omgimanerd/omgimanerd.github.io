b61b1c15665ec6f7b5575328cefc012969c591e9
Alvin Lin <alvin.lin@stuypulse.com>
2015-09-25 22:27:44 -0400

    using closure compiler to compile javascript

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,48 @@
  */
 
 var gulp = require('gulp');
-var less = require('gulp-less');
+
 var autoprefixer = require('gulp-autoprefixer');
+var closureCompiler = require('gulp-closure-compiler');
+var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
 var notify = require('gulp-notify');
+var rename = require('gulp-rename');
+
+gulp.task('default', ['js', 'less']);
+
+gulp.task('js', function() {
+  return gulp.src('./scripts/*.js')
+    .pipe(closureCompiler({
+      compilerPath: 'bower_components/closure-compiler/compiler.jar',
+      fileName: 'minified.js'
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(notify('JS compiled and minified'));
+});
 
 gulp.task('less', function() {
   return gulp.src('./style/styles.less')
     .pipe(less({ compress: true}))
     .pipe(autoprefixer({ browsers: ['last 10 versions']}))
     .pipe(minifyCss())
-    .pipe(gulp.dest('./style'))
+    .pipe(rename(function(path) {
+      path.basename = 'minified';
+      path.extname = '.css';
+    }))
+    .pipe(gulp.dest('./dist'))
     .pipe(notify('LESS compiled and minified'));
 });
 
+gulp.task('watch-js', function() {
+  gulp.watch('./scripts/*.js', ['js']);
+});
+
 gulp.task('watch-less', function() {
+  gulp.watch('./style/*.less', ['less']);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./scripts/*.js', ['js']);
   gulp.watch('./style/*.less', ['less']);
 });
