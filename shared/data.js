05fbc971242b38e43106eb2cf99b40722ce205aa
omgimanerd <alvin.lin.dev@gmail.com>
2016-06-21 14:15:23 -0400

    more info

--- shared/data.js
+++ shared/data.js
@@ -31,40 +31,33 @@ module.exports = {
       value: 9
     }
   ],
-  experience: [
+  work: [
     {
-      icon: 'school',
-      title: 'RoboMindTech - Fresh Meadows, NY',
-      content: 'Mentored First Lego League (FLL) and World Robot Olympiad (WRO) robotics teams. Created robotics education content for distribution in Singapore and Taiwan. Taught cryptography, web security, web design, game design, HTML, CSS, JavaScript, Java, Python, and Processing.'
+      name: 'SchoolYourself Inc',
+      description: 'Developed interactive geometry curriculum in JavaScript used by over 55,000 people in more than 100 countries around the world. Integrated educational content with commercial Learning Management Systems using the Learning Tools Interoperability (LTI) standard.',
+      link: 'https://schoolyourself.org',
+      linkText: 'Homepage',
+      image: '/public/img/schoolyourself.png'
     },
-    {
-      icon: 'web',
-      title: 'SchoolYourself - Manhattan, NY',
-      content: 'Developed interactive videos for geometry curriculum in JavaScript used by over 55,000 people in more than 100 countries around the world. Integrated educational content with commercial Learning Management Systems using the Learning Tools Interoperability (LTI) standard.'
-    },
-    {
-      icon: 'school',
-      title: 'Florentine School of Music, Art, and Academics - Manhattan, NY',
-      content: 'Taught mathematics and music to 3rd, 4th, and 5th graders. Streamlined curriculum for better information retention by students.'
-    }
-  ],
-  apps: [
     {
       name: 'Bouncy Square',
       description: 'Bouncy Square is an addictive casual game involving hand-eye coordination. Tap the left and right sides of the screen to rotate the square clockwise and counterclockwise. Tilt your device to move the square left and right. To bounce, you have to land on the platform with the side of the square that has the same color as the platform. Get as far as you can!',
       link: 'https://play.google.com/store/apps/details?id=io.github.omgimanerd.bouncysquare',
+      linkText: 'Get It Here!',
       image: '/public/img/bouncysquare.png'
     },
     {
       name: 'Shockwave',
       description: 'Shockwave is a two-player game in which both players can tap the screen to create a shockwave that will push the ball away from it. To prevent either player from simply mashing the screen, tapping is only allowed if the other player has tapped or after two seconds have elapsed. The goal of the game is to push the ball into the opposing player\'s goal.',
       link: 'https://play.google.com/store/apps/details?id=io.github.omgimanerd.shockwave',
+      linkText: 'Get It Here!',
       image: '/public/img/shockwave.png'
     },
     {
       name: 'Tap',
       description: 'Tap is a game of speed where balls of four different colors will appear and move in a wave across the screen. You must tap the ball when it is on top of the stripe that corresponds to it\'s color. You lose when you let a ball reach the other side of the screen or if you tap one when it is on top of the wrong color.',
       link: 'https://play.google.com/store/apps/details?id=io.github.omgimanerd.tap',
+      linkText: 'Get It Here!',
       image: '/public/img/tap.png'
     }
   ]
