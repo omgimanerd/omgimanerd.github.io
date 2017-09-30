4bf5b90536f9ba45ff6bc1865911f094164906f0
omgimanerd <alvin@omgimanerd.tech>
2017-09-30 04:05:43 -0400

    Refactored notes fetching to an external module

---
+++ server/notes.js
@@ -0,0 +1,101 @@
+/**
+ * @fileoverview This file handles the fetching, caching, and updating of the
+ * LaTeX notes. It is contains the functionality needed by notesRouter.js
+ * @author alvin@omgimanerd.tech (Alvin Lin)
+ */
+
+const _ = require('lodash')
+const Promise = require('bluebird')
+const exec = Promise.promisify(require('child_process').exec)
+const fs = Promise.promisifyAll(require('fs'))
+const path = require('path')
+
+const config = require('../config')
+
+// Local cache
+const cache = {}
+
+/**
+ * Invokes the commands in the notes directory to pull and update the notes.
+ * @return {Promise}
+ */
+const updateNotes = () => {
+  return exec('git pull', { cwd: config.NOTES_PATH }).then(() => {
+    return exec('gulp clean', { cwd: config.NOTES_PATH })
+  }).then(() => {
+    cache.expired = true
+    return exec('gulp latex', { cwd: config.NOTES_PATH })
+  })
+}
+
+/**
+ * Given a latex file directory name, this function separates out the parts by
+ * delimiter and formats it into a displayable format.
+ * @param {string} directory The class directory name
+ * @return {string}
+ */
+const formatClassName = directory => {
+  const parts = directory.split('_')
+  const label = `${parts[0].toUpperCase().replace('-', ' ')}: `
+  const className = parts[1].split('-').map(_.capitalize).join(' ')
+  return label + className
+}
+
+/**
+ * Given the class directory and the tex file name, this function returns
+ * the path to file relative to the root.
+ * @param {string} directory The class directory name
+ * @param {string} file The tex file name
+ * @return {string}
+ */
+const formatFilePath = (directory, file) => {
+  return path.join(
+    'notes/latex', directory, 'output', file.replace('.tex', '.pdf'))
+}
+
+/**
+ * Fetches and creates the hierarchy of notes for caching. This is invoked
+ * during server startup and the internal cache is updated each time the notes
+ * update endpoint is hit.
+ * @return {Promise}
+ */
+const getNotes = () => {
+  /**
+   * We first check if the notes hierarchy has been previously requested and
+   * cached. The cache is considered expired if updateNotes() was invoked, in
+   * which case we should update the cache with the new notes hierarchy.
+   */
+  if (cache.data && !cache.expired) {
+    return Promise.resolve(cache)
+  }
+  // Iterate through each directory in the latex folder.
+  return fs.readdirAsync(config.NOTES_PATH).then(directories => {
+    // For each directory
+    return Promise.all(directories.map(directory => {
+      const dirPath = path.join(config.NOTES_PATH, directory)
+      // Read the names of all the files in the directory
+      return fs.readdirAsync(dirPath).then(files => {
+        /**
+         * Filter out all the .tex files and infer the names of all the
+         * .pdf files. Return an object which the template will use to
+         * render the expandable accordion.
+         */
+        return files.filter(file => file.endsWith('.tex')).map(file => {
+          return {
+            filename: file,
+            path: formatFilePath(directory, file)
+          }
+        })
+      }).then(data => {
+        return { [formatClassName(directory)]: data }
+      })
+    })).reduce(_.merge)
+  }).tap(data => {
+    cache.data = data
+  })
+}
+
+module.exports = exports = {
+  updateNotes,
+  getNotes
+}
--- server/notesRouter.js
+++ server/notesRouter.js
@@ -4,132 +4,52 @@
  * @author alvin.lin.dev@gmail.com (Alvin Lin)
  */
 
-const _ = require('lodash')
-const Promise = require('bluebird')
 const express = require('express')
-const fs = Promise.promisifyAll(require('fs'))
 const githubWebhook = require('github-webhook-middleware')
-const path = require('path')
-
-const exec = Promise.promisify(require('child_process').exec)
 
 const config = require('../config')
 const loggers = require('./loggers')
-
-/**
- * Invokes the commands in the notes directory to pull and update the notes.
- * @return {Promise}
- */
-const updateNotes = () => {
-  return exec('git pull', { cwd: config.NOTES_PATH }).then(() => {
-    return exec('gulp clean', { cwd: config.NOTES_PATH })
-  }).then(() => {
-    return exec('gulp latex', { cwd: config.NOTES_PATH })
-  })
-}
-
-/**
- * Given a latex file directory name, this function separates out the parts by
- * delimiter and formats it into a displayable format.
- * @param {string} directory The class directory name
- * @return {string}
- */
-const formatClassName = directory => {
-  const parts = directory.split('_')
-  const label = `${parts[0].toUpperCase().replace('-', ' ')}: `
-  const className = parts[1].split('-').map(_.capitalize).join(' ')
-  return label + className
-}
-
-/**
- * Given the class directory and the tex file name, this function returns
- * the path to file relative to the root.
- * @param {string} directory The class directory name
- * @param {string} file The tex file name
- * @return {string}
- */
-const formatFilePath = (directory, file) => {
-  return path.join(
-    'notes/latex', directory, 'output', file.replace('.tex', '.pdf'))
-}
-
-/**
- * Fetches and creates the hierarchy of notes for caching. This is invoked
- * during server startup and the internal cache is updated each time the notes
- * update endpoint is hit.
- * @return {Promise}
- */
-const getNotes = () => {
-  // Iterate through each directory in the latex folder.
-  return fs.readdirAsync(config.NOTES_PATH).then(directories => {
-    // For each directory
-    return Promise.all(directories.map(directory => {
-      const dirPath = path.join(config.NOTES_PATH, directory)
-      // Read the names of all the files in the directory
-      return fs.readdirAsync(dirPath).then(files => {
-        /**
-         * Filter out all the .tex files and infer the names of all the
-         * .pdf files. Return an object which the template will use to
-         * render the expandable accordion.
-         */
-        return files.filter(file => file.endsWith('.tex')).map(file => {
-          return {
-            filename: file,
-            path: formatFilePath(directory, file)
-          }
-        })
-      }).then(data => {
-        return { [formatClassName(directory)]: data }
-      })
-    })).reduce(_.merge)
-  })
-}
+const notes = require('./notes')
 
 /**
  * Fetches and stores the analytics data for caching.
  * @return {Promise}
  */
-const getAnalytics = () => {
-  return fs.readFileAsync(config.ANALYTICS_LOG, 'utf-8').then(data => {
-    if (!data) {
-      return []
-    }
-    return data.trim().split('\n').map(JSON.parse)
-  })
-}
+// const getAnalytics = () => {
+//   return fs.readFileAsync(config.ANALYTICS_LOG, 'utf-8').then(data => {
+//     if (!data) {
+//       return []
+//     }
+//     return data.trim().split('\n').map(JSON.parse)
+//   })
+// }
 
 /**
  * TODO: Refactor the caches so they're not part of the router file
  */
-let analyticsCache = null
-let analyticsExpirationTime = 0
-let notesCache = null
+// let analyticsCache = null
+// let analyticsExpirationTime = 0
 
 const router = express.Router()
 
 router.get('/', (request, response) => {
-  if (notesCache) {
-    response.render('notes', { notes: notesCache })
-  } else {
-    getNotes().then(data => {
-      notesCache = data
-      response.render('notes', { notes: notesCache })
-    })
-  }
+  notes.getNotes().then(data => {
+    response.render('notes', { notes: data })
+  })
 })
 
-router.post('/analytics', (request, response) => {
-  const currentTime = Date.now()
-  if (analyticsCache && analyticsExpirationTime > currentTime) {
-    response.send(analyticsCache)
-  } else {
-    getAnalytics().then(data => {
-      analyticsCache = data
-      analyticsExpirationTime = currentTime + 600000
-      response.send(data)
-    })
-  }
-})
+// router.post('/analytics', (request, response) => {
+//   const currentTime = Date.now()
+//   if (analyticsCache && analyticsExpirationTime > currentTime) {
+//     response.send(analyticsCache)
+//   } else {
+//     getAnalytics().then(data => {
+//       analyticsCache = data
+//       analyticsExpirationTime = currentTime + 600000
+//       response.send(data)
+//     })
+//   }
+// })
 
 router.use('/latex', loggers.analyticsLoggerMiddleware)
 
@@ -144,8 +64,12 @@ const middleware = githubWebhook({
 })
 router.post('/update', middleware, (request, response) => {
   if (request.headers['x-github-event'] === 'push') {
-    notesCache = getNotes()
-    response.send('OK')
+    notes.updateNotes().then(() => {
+      response.send('OK')
+    }).catch(error => {
+      loggers.logError(error)
+      response.status(500).send('Something failed!')
+    })
   } else {
     response.status(200).end()
   }
@@ -157,12 +81,10 @@ router.post('/update', middleware, (request, response) => {
  */
 router.get('/update', (request, response) => {
   if (!config.PRODUCTION) {
-    updateNotes(error => {
-      if (error) {
-        response.send(error)
-      } else {
-        response.send('Update complete!')
-      }
+    notes.updateNotes().then(() => {
+      response.redirect('/notes')
+    }).catch(error => {
+      response.status(500).send(error)
     })
   } else {
     response.redirect('/notes')
