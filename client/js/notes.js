3cf3e72d496c1be5a7e2d843e492d0b52e535f9c
omgimanerd <alvin@omgimanerd.tech>
2017-10-01 00:40:58 -0400

    analytics d3 c3

--- client/js/notes.js
+++ client/js/notes.js
@@ -7,8 +7,17 @@ require('./base')
 
 const $ = require('jquery')
 // eslint-disable-next-line no-unused-vars
+const d3 = require('d3')
+// eslint-disable-next-line no-unused-vars
+const c3 = require('c3')
+// eslint-disable-next-line no-unused-vars
 const Materialize = require('materialize-css')
 
 $(document).ready(() => {
+  $('.modal').modal()
   $('#notes-modal').modal('open')
+
+  $.post('/notes/analytics', data => {
+    console.log(data)
+  })
 })
