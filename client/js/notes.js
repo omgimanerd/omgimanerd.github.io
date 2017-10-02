5f309dcec55a0bfe189266bc48befd9665c385b9
omgimanerd <alvin@omgimanerd.tech>
2017-10-01 21:53:27 -0400

    Analytics Chart in development

--- client/js/notes.js
+++ client/js/notes.js
@@ -8,7 +8,6 @@ require('./base')
 const $ = require('jquery')
 // eslint-disable-next-line no-unused-vars
 const d3 = require('d3')
-// eslint-disable-next-line no-unused-vars
 const c3 = require('c3')
 // eslint-disable-next-line no-unused-vars
 const Materialize = require('materialize-css')
@@ -18,6 +17,18 @@ $(document).ready(() => {
   $('#notes-modal').modal('open')
 
   $.post('/notes/analytics', data => {
-    console.log(data)
+    const trafficChart = c3.generate({
+      bindto: '#traffic',
+      axis: {
+        x: { padding: 0, type: 'timeseries' },
+        y: { label: 'Requests', min: 0, padding: 0 }
+      },
+      data: {
+        x: 'date',
+        columns: data.traffic
+      },
+      points: { show: false },
+      padding: { right: 25 }
+    })
   })
 })
