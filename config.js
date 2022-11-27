a12978977ab61b437284390cab7794a87caaec35
Alvin Lin <alvin@omgimanerd.tech>
2022-11-26 19:37:37 -0500

    Remove semicolons

--- config.js
+++ config.js
@@ -5,30 +5,30 @@
  * @author alvin@omgimanerd.tech (Alvin Lin)
  */
 
-import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
-import url from 'url';
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+import url from 'url'
 
 const CONST_AVAILABLE =
-  fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK;
+  fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-dotenv.config();
+dotenv.config()
 
-const dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const NOTES_PATH = path.join(dirname, 'rit', 'latex');
-const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET;
+const dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const NOTES_PATH = path.join(dirname, 'rit', 'latex')
+const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET
 
 fs.access(NOTES_PATH, CONST_AVAILABLE, error => {
   if (error) {
-    throw new Error('Unable to access /notes, please clone the notes repo.');
+    throw new Error('Unable to access /notes, please clone the notes repo.')
   }
-});
+})
 
 export default {
   PORT,
   NOTES_PATH,
   GITHUB_WEBHOOK_SECRET
-};
+}
